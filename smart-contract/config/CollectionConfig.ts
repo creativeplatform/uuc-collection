316b9fb1ea38bb886d82f46ede9e86c54c3036a8
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-07-15 10:29:16 -0400

    updates to contract

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,10 +9,10 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: 'UucNftToken',
+  tokenName: 'Urban Uprise Crew',
+  tokenSymbol: 'UUC',
+  hiddenMetadataUri: 'ipfs://QmNZvkSgyDVUD95FmwNk3mzks6oTXm4YWQ5T2y2nzyADRE/Test.png',
   maxSupply: 10000,
   whitelistSale: {
     price: 0.05,
@@ -27,7 +27,7 @@ const CollectionConfig: CollectionConfigInterface = {
     maxMintAmountPerTx: 5,
   },
   contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  marketplaceIdentifier: 'urban-uprise-crew',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
--- smart-contract/contracts/UucNftToken.sol
+++ smart-contract/contracts/UucNftToken.sol
@@ -2,20 +2,20 @@
 
 pragma solidity >=0.8.9 <0.9.0;
 
-import 'erc721a/contracts/extensions/ERC721AQueryable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
-import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import "erc721a/contracts/extensions/ERC721AQueryable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
+contract UucNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
 
   using Strings for uint256;
 
   bytes32 public merkleRoot;
   mapping(address => bool) public whitelistClaimed;
 
-  string public uriPrefix = '';
-  string public uriSuffix = '.json';
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
   string public hiddenMetadataUri;
   
   uint256 public cost;
@@ -41,29 +41,29 @@ contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
   }
 
   modifier mintCompliance(uint256 _mintAmount) {
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, 'Invalid mint amount!');
-    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(totalSupply() + _mintAmount <= maxSupply, "Max supply exceeded!");
     _;
   }
 
   modifier mintPriceCompliance(uint256 _mintAmount) {
-    require(msg.value >= cost * _mintAmount, 'Insufficient funds!');
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
     _;
   }
 
   function whitelistMint(uint256 _mintAmount, bytes32[] calldata _merkleProof) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
     // Verify whitelist requirements
-    require(whitelistMintEnabled, 'The whitelist sale is not enabled!');
-    require(!whitelistClaimed[_msgSender()], 'Address already claimed!');
+    require(whitelistMintEnabled, "The whitelist sale is not enabled!");
+    require(!whitelistClaimed[_msgSender()], "Address already claimed!");
     bytes32 leaf = keccak256(abi.encodePacked(_msgSender()));
-    require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), 'Invalid proof!');
+    require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), "Invalid proof!");
 
     whitelistClaimed[_msgSender()] = true;
     _safeMint(_msgSender(), _mintAmount);
   }
 
   function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
-    require(!paused, 'The contract is paused!');
+    require(!paused, "The contract is paused!");
 
     _safeMint(_msgSender(), _mintAmount);
   }
@@ -77,7 +77,7 @@ contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
   }
 
   function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
-    require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
+    require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
 
     if (revealed == false) {
       return hiddenMetadataUri;
@@ -86,7 +86,7 @@ contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
         ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
-        : '';
+        : "";
   }
 
   function setRevealed(bool _state) public onlyOwner {
@@ -126,18 +126,18 @@ contract YourNftToken is ERC721AQueryable, Ownable, ReentrancyGuard {
   }
 
   function withdraw() public onlyOwner nonReentrant {
-    // This will pay HashLips Lab Team 5% of the initial sale.
+    // This will pay Creative Organization DAO 50% of the initial sale.
     // By leaving the following lines as they are you will contribute to the
-    // development of tools like this and many others.
+    // development of Creative platform and future projects.
     // =============================================================================
-    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 5 / 100}('');
+    (bool hs, ) = payable(0xF9755875e93f5CE66B0Fc6a4763f20Ae0cb90d18).call{value: address(this).balance * 50 / 100}("");
     require(hs);
     // =============================================================================
 
     // This will transfer the remaining contract balance to the owner.
     // Do not remove this otherwise you will not be able to withdraw the funds.
     // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}('');
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
     // =============================================================================
   }
